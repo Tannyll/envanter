1b0f97d95aab969332cec83e3b4167c7071c2b93
Serdar EMIRCI <serdar@emirci.com>
2017-10-09 11:51:46 +0300

    Initial commit

---
+++ src/main/java/com/emirci/envanter/controller/InventoryController.java
@@ -0,0 +1,136 @@
+package com.emirci.envanter.controller;
+
+import com.emirci.envanter.Repository.DepartmentRepository;
+import com.emirci.envanter.Repository.InventoryRepository;
+import com.emirci.envanter.Repository.InventoryTypeRepository;
+import com.emirci.envanter.Repository.TrademarkRepository;
+import com.emirci.envanter.model.AppUser;
+import com.emirci.envanter.model.Inventory;
+import com.emirci.envanter.service.impl.MessageByLocaleServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * .Created by serdaremirci on 9/25/17.
+ */
+@Controller
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    private InventoryRepository repo;
+    private TrademarkRepository trademarkRepository;
+    private InventoryTypeRepository inventoryTypeRepository;
+    private DepartmentRepository departmentRepository;
+    @Value("${app.name:test}")
+    private String message = "message";
+
+    @Inject
+    MessageByLocaleServiceImpl messageByLocaleService;
+
+    public InventoryController(InventoryRepository repo, TrademarkRepository trademarkRepository, InventoryTypeRepository inventoryTypeRepository, DepartmentRepository departmentRepository) {
+        this.repo = repo;
+        this.trademarkRepository = trademarkRepository;
+        this.inventoryTypeRepository = inventoryTypeRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+
+    @RequestMapping("/list")
+    public ModelAndView inventoryList() {
+
+        ModelAndView model = new ModelAndView("inventory/inventoryList");
+        model.addObject("message", this.message);
+        model.addObject("inventoryList", repo.findAll());
+
+        return model;
+    }
+
+    @RequestMapping(value = "/newInventory", method = RequestMethod.GET)
+    public ModelAndView newInventory() {
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.addObject("formBean", new Inventory());
+        modelAndView.setViewName("update");
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/update/{id}")
+    public ModelAndView update(@PathVariable Long id) {
+
+        ModelAndView modelAndView = new ModelAndView("inventory/update");
+
+        modelAndView.addObject("formBeanTrademark",trademarkRepository.findAll());
+        modelAndView.addObject("formBeanInventoryType",inventoryTypeRepository.findAll());
+        modelAndView.addObject("formBeanDepartment",departmentRepository.findAll());
+        modelAndView.addObject("formBean", repo.getOne(id));
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public ModelAndView update(@ModelAttribute("formBean") @Valid Inventory inventory, final BindingResult bindingResult) {
+
+        DateFormat dateFormat = new SimpleDateFormat();
+        Date date = new Date();
+
+        //bindingResult.rejectValue("model", messageByLocaleService.getMessage("inventory.field.model"),"There is already a user registered with the email provided");
+
+        inventory.setInsertDate(date);
+        inventory.setUserId("1");
+        inventory.setInsertUserId("1");
+
+        ModelAndView modelAndView = new ModelAndView("inventory/inventoryList");
+        modelAndView.addObject("serverTime", Calendar.getInstance().getTime());
+
+        if (bindingResult.hasErrors()) {
+            modelAndView.addObject("formBean", inventory);
+            modelAndView.addObject("msg", messageByLocaleService.getMessage("form.data.NotSaved"));
+            return modelAndView;
+
+        } else {
+            repo.save(inventory);
+            modelAndView.addObject("msg", messageByLocaleService.getMessage("form.data.saved"));
+            modelAndView.addObject("formBean", new AppUser());
+        }
+
+        //modelAndView.setViewName("redirect:/");
+
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public ModelAndView delete(HttpServletRequest request) {
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        Long inventoryId = Long.parseLong(request.getParameter("inventoryId"));
+
+        repo.delete(inventoryId);
+
+
+        modelAndView.setViewName("redirect:/");
+
+        return modelAndView;
+    }
+
+}
+
---
+++ src/main/java/com/emirci/envanter/controller/UserController.java
@@ -0,0 +1,118 @@
+package com.emirci.envanter.controller;
+
+
+import com.emirci.envanter.Repository.InventoryRepository;
+import com.emirci.envanter.model.AppUser;
+import com.emirci.envanter.service.SecurityService;
+import com.emirci.envanter.service.UserService;
+import com.emirci.envanter.service.impl.MessageByLocaleServiceImpl;
+import com.emirci.envanter.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import java.util.Calendar;
+
+/**
+ * Created by serdaremirci on 9/19/17.
+ */
+@Controller
+public class UserController {
+
+
+    InventoryRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @Inject
+    MessageByLocaleServiceImpl messageByLocaleService;
+
+    public UserController(InventoryRepository repository) {
+        this.repository = repository;
+
+    }
+
+    //https://medium.com/@gustavo.ponce.ch/spring-boot-spring-mvc-spring-security-mysql-a5d8545d837d
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration() {
+
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        AppUser user = new AppUser();
+        modelAndView.addObject("formBean", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@ModelAttribute("formBean") @Valid AppUser user, final BindingResult bindingResult) {
+
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("serverTime", Calendar.getInstance().getTime());
+
+        AppUser userExists = userService.findUserByEmail(user.getEmail());
+
+        userValidator.validate(user, bindingResult);
+
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", messageByLocaleService.getMessage("form.validate.duplicate.username"),
+                            "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.addObject("formBean", user);
+            modelAndView.addObject("msg", messageByLocaleService.getMessage("form.data.NotSaved"));
+            modelAndView.setViewName("registration");
+
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("msg", messageByLocaleService.getMessage("form.data.saved"));
+            modelAndView.addObject("formBean", new AppUser());
+            modelAndView.setViewName("registration");
+
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/home", method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        AppUser user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("admin/home");
+        return modelAndView;
+    }
+
+}
